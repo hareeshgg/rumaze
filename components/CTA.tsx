7021a67ace2a35daf56c9c8de88e939fc586db85
hareeshgg <hareeshgurung@gmail.com>
2026-04-11 22:54:27 +0530

    update cta button

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,78 @@
+"use client";
+
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const CTA = () => {
+  const circleRef = useRef<HTMLDivElement>(null);
+  const textPathRef = useRef<SVGTextPathElement>(null);
+
+  useGSAP(
+    () => {
+      if (circleRef.current) {
+        gsap.to(circleRef.current, {
+          rotation: 360,
+          duration: 12,
+          repeat: -1,
+          ease: "none",
+        });
+      }
+    },
+    { scope: circleRef },
+  );
+
   return (
-    <div className="fixed bottom-0 right-0 mb-6 mr-6 lg:mb-12 lg:mr-12 h-18 w-18 lg:h-24 lg:w-24  bg-white rounded-full">
-      Hi
+    <div
+      className="fixed bottom-6 right-6 lg:bottom-12 lg:right-12 z-50 group cursor-pointer"
+      onClick={() => window.open("https://calendly.com", "_blank")} // Placeholder link
+    >
+      <div
+        ref={circleRef}
+        className="relative h-18 w-18 lg:h-24 lg:w-24 xl:h-32 xl:w-32 flex items-center justify-center bg-white rounded-full shadow-2xl transition-transform duration-500 group-hover:scale-110"
+      >
+        {/* Circular Text */}
+        <svg
+          viewBox="0 0 100 100"
+          className="absolute inset-0 w-full h-full p-2"
+        >
+          <defs>
+            <path
+              id="circlePath"
+              d="M 50, 50 m -37, 0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0"
+            />
+          </defs>
+          <text className="text-[10px] lg:text-[11px] xl:text-[12px] font-bold uppercase tracking-[0.2em] fill-black">
+            <textPath href="#circlePath" startOffset="0%">
+              Book A Call • Book A Call •
+            </textPath>
+          </text>
+        </svg>
+
+        {/* Center Icon (Custom Asterisk) */}
+        <div className="absolute inset-0 flex items-center justify-center p-8 lg:p-10">
+          <svg
+            viewBox="0 0 24 24"
+            className="w-full h-full fill-black transition-transform duration-500 group-hover:rotate-90"
+          >
+            <path
+              d="M12 0L14.5 9H24L16.5 14.5L19 23.5L12 18L5 23.5L7.5 14.5L0 9H9.5L12 0Z"
+              className="hidden"
+            />{" "}
+            {/* Reference star */}
+            {/* Creating the thick 6-point asterisk from image */}
+            <g transform="translate(12, 12)">
+              {[0, 60, 120, 180, 240, 300].map((angle) => (
+                <path
+                  key={angle}
+                  d="M -1.5 0 C -1.5 -10, 1.5 -10, 1.5 0 L 1.5 8 C 1.5 10, -1.5 10, -1.5 8 Z"
+                  transform={`rotate(${angle})`}
+                />
+              ))}
+            </g>
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,18 +1,73 @@
+"use client";
+import { useRef } from "react";
+import SplitText from "./SplitText";
+
 const Hero = () => {
+  const subtitleRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div className="relative h-screen w-full flex flex-col items-start justify-end xl:flex-row xl:items-end xl:justify-start ">
+    <div className="relative h-dvh w-full flex flex-col items-start justify-end xl:flex-row xl:items-end xl:justify-start">
       <h1 className="text-white text-5xl md:text-7xl lg:text-8xl xl:text-9xl pl-8 md:pl-24 lg:pl-28 pb-8 lg:pb-12">
-        <span className="italic pl-4 md:pl-0"> Elegance</span>
+        <SplitText
+          text="Elegance"
+          className="italic pl-4 md:pl-0"
+          delay={100}
+          tag="span"
+          textAlign="left"
+          trigger={subtitleRef}
+        />
         <br />
-        <span className="md:pl-20 lg:pl-38 xl:pl-48">is not added</span> <br />
+        <SplitText
+          text="is not added"
+          className="md:pl-20 lg:pl-38 xl:pl-48"
+          delay={100}
+          tag="span"
+          textAlign="left"
+          to={{ opacity: 1, y: 0, delay: 0.5 }}
+          trigger={subtitleRef}
+        />
+        <br />
         <span className="pl-8 md:pl-8 lg:pl-12">
-          but <span className="italic ">designed</span>{" "}
+          <SplitText
+            text="but"
+            delay={100}
+            tag="span"
+            textAlign="left"
+            to={{ opacity: 1, y: 0, delay: 1.0 }}
+            trigger={subtitleRef}
+          />{" "}
+          <SplitText
+            text="designed"
+            className="italic text-white"
+            delay={100}
+            tag="span"
+            textAlign="left"
+            to={{ opacity: 1, y: 0, delay: 1.2 }}
+            trigger={subtitleRef}
+          />
         </span>
       </h1>
-      <p className="text-sm md:text-base lg:text-lg text-white pl-16 md:pl-24 lg:pl-32 xl:pl-0 pb-4 lg:pb-16 xl:pb-8">
-        YOU BUILD YOUR DREAM. <br />
-        WE DESIGN THE REALITY.
-      </p>
+      <div 
+        ref={subtitleRef}
+        className="text-sm md:text-base lg:text-lg text-white pl-16 md:pl-24 lg:pl-32 xl:pl-0 pb-4 lg:pb-16 xl:pb-16"
+      >
+        <SplitText
+          text="YOU BUILD YOUR DREAM."
+          className="block"
+          delay={50}
+          tag="p"
+          textAlign="left"
+          to={{ opacity: 1, y: 0, delay: 1.5 }}
+        />
+        <SplitText
+          text="WE DESIGN THE REALITY."
+          className="block"
+          delay={50}
+          tag="p"
+          textAlign="left"
+          to={{ opacity: 1, y: 0, delay: 1.8 }}
+        />
+      </div>
     </div>
   );
 };
